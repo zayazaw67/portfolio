c68af3b1ec10fbacc2092c3a7e7892fa7b8d6bb5
Zaya <zayazaw67@gmail.com>
2019-12-11 03:19:33 -0600

    Updates

---
+++ precache-manifest.66fa4a149308ea55cf7cf80fbf6f471d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59681199f32ca052d9db19bbe9165ac7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4c42f7af2e75ef85f8fb",
+    "url": "/portfolio/static/css/2.3966abe9.chunk.css"
+  },
+  {
+    "revision": "ee2ac70d908e341d368e",
+    "url": "/portfolio/static/css/main.4428ed06.chunk.css"
+  },
+  {
+    "revision": "4c42f7af2e75ef85f8fb",
+    "url": "/portfolio/static/js/2.a0db1b9f.chunk.js"
+  },
+  {
+    "revision": "ee2ac70d908e341d368e",
+    "url": "/portfolio/static/js/main.fcddcf50.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "b110719f2aaf7db55a6f9da0e6702899",
+    "url": "/portfolio/static/media/bamazon.b110719f.png"
+  },
+  {
+    "revision": "56cd5e308610ea019370cc47bfcc08d4",
+    "url": "/portfolio/static/media/bamazonpreview.56cd5e30.gif"
+  },
+  {
+    "revision": "22735574b0e152f4f94d407859520dba",
+    "url": "/portfolio/static/media/bg.22735574.png"
+  },
+  {
+    "revision": "eaaab2b6274e18bcac80bd731cbe59dd",
+    "url": "/portfolio/static/media/budgettracker.eaaab2b6.png"
+  },
+  {
+    "revision": "f9c3dd1c0eec156be49f1981edb54564",
+    "url": "/portfolio/static/media/clickgame.f9c3dd1c.png"
+  },
+  {
+    "revision": "4f683767867ef09945b6133ee876ade1",
+    "url": "/portfolio/static/media/clickpreview.4f683767.gif"
+  },
+  {
+    "revision": "3592d3dd765b2d0b1fd19bc2490e0068",
+    "url": "/portfolio/static/media/fraigslist.3592d3dd.png"
+  },
+  {
+    "revision": "0c1d90c617270f0d62144f6760b73d36",
+    "url": "/portfolio/static/media/fraigspreview.0c1d90c6.gif"
+  },
+  {
+    "revision": "c10d7eb6c4bbc530a36d92c01cf81b46",
+    "url": "/portfolio/static/media/trackerpreview.c10d7eb6.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eebab54f7e80adf5194c743fb4e32dc3.js"
+  "/portfolio/precache-manifest.66fa4a149308ea55cf7cf80fbf6f471d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
